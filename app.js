4c88ddd39eaecc6e0ac5b4c022df202d053d0b8c
NESTOR EMILIO PEREZ SANCHEZ <ing.nestor.sanz@gmail.com>
2020-09-07 19:19:29 -0500

    Actuaizar

--- app.js
+++ app.js
@@ -2,32 +2,31 @@
 var express = require('express');
 var service = require('./modules/service');
 var config = require('./config');
-
-var app = express();
 var bodyParser = require('body-parser');
 var ip = require('ip');
+var ipRangeCheck = require("ip-range-check");
+
+var app = express();
+
 
 app.use( function(req, res, next) {
     var invalidIp = false;
     console.log('middleware');
-    var blackList = config.BLACKLIST.split(",");
-    console.log(blackList);
-    console.log('ip-peticion: ', ip.address());
-    
+    var ipReq = ip.address();
+    var blackList =  config.BLACKLIST.split(",");
+
+    console.log('MyIP: ', ip.address());
     //Revisar grupo de IPS de  BLACKLIST
     for (var i = 0, len = blackList.length; i < len; i++) {
         console.log('ip--- ', blackList[i]);
-        if (blackList[i].toString() == ip.address()) {
-            invalidIp = true;
+        if (ipRangeCheck(blackList[i],  ip.address())) {
+            console.log(`IP address blacklist ${ip.address()} has tried to access the service`);
+            
+        }else {
+            next();
         }
     }
-    //Mensaje en el LOG si la IP no es válida
-    if (invalidIp) {
-        console.log(`IP address ${ip.address()} has tried to access the service`);
-        res.send('Error de IP');
-    }
-
-    next();
+    
 });
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({
--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
 
     PORT: process.env.PORT || 5000,
-    BLACKLIST: process.env.BLACKLIST || "192.168.1.133, 192.168.1.132",
+    BLACKLIST: process.env.BLACKLIST || "162.168.1.138,192.168.1.132",
 
 }
